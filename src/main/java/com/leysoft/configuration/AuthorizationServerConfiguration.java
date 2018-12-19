cf83e3fdcc02eedfa072cebb4b1d630c78db3895
Will Leyton <will.leyton@rappi.com>
2018-12-19 11:31:28 -0500

    Add logif for in memory clients and users

--- src/main/java/com/leysoft/configuration/AuthorizationServerConfiguration.java
+++ src/main/java/com/leysoft/configuration/AuthorizationServerConfiguration.java
@@ -1,13 +1,58 @@
 
 package com.leysoft.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import com.leysoft.util.GrantTypes;
+import com.leysoft.util.Scope;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
-    
-    
+
+    @Value(
+            value = "${oauth2.client.id}")
+    private String clientId;
+
+    @Value(
+            value = "${oauth2.client.secret}")
+    private String secretClient;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient(clientId).secret(passwordEncoder.encode(secretClient))
+                .authorizedGrantTypes(GrantTypes.PASSWORD_CREDENTIALS.toString())
+                .scopes(Scope.READ.toString());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()")
+                .passwordEncoder(passwordEncoder);
+    }
 }
---
+++ src/main/java/com/leysoft/util/Role.java
@@ -0,0 +1,17 @@
+
+package com.leysoft.util;
+
+public enum Role {
+    USER("USER"), ADMIN("ADMIN");
+
+    private String value;
+
+    private Role(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
