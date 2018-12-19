28a4f59299bfddc9a40d64fd05c9d9bf2152328f
Will Leyton <will.leyton@rappi.com>
2018-12-19 08:35:47 -0500

    Add AuthorizationServerConfiguration

---
+++ src/main/java/com/leysoft/SbOauth2AuthServerApplication.java
@@ -0,0 +1,14 @@
+package com.leysoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbOauth2AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbOauth2AuthServerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/leysoft/SbOauth2AuthServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leysoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbOauth2AuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
