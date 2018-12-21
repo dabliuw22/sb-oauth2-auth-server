2cb1703d417022b6eb349e9408699d2ef468b586
Will Leyton <will.leyton@rappi.com>
2018-12-21 15:10:27 -0500

    Add auth-code logic

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Oauth2 Authorization Server
 
-Authorization server that allows you to store clients in memory and generate JWT tokens, using the password grants.
+Authorization server that allows you to store clients in memory and generate JWT tokens, using the authorization_code grants.
 
 1. Requirements:
 	* Java >= 1.8
@@ -11,17 +11,20 @@ Authorization server that allows you to store clients in memory and generate JWT
 git clone https://github.com/dabliuw22/sb-oauth2-auth-server.git
 ```
 
-3. Switch to the `password-memory-client` branch:
+3. Switch to the `auth-code-memory-client` branch:
 
 ```
-git checkout password-memory-client
+git checkout auth-code-memory-client
 ```
 
 4. Running the project.
 
-5. Make request to get the bearer token to the endpoint `/oauth/token`.
+5. Obtain authorization code [here](http://localhost:9090/oauth/authorize?client_id=clientapp&redirect_uri=http://localhost:9091&response_type=code&scope=read)
+   and copy the code. `username: user` and `password: user.password`
+
+6. Make request to get the bearer token to the endpoint `/oauth/token`.
 
 ```
 curl -X POST -u clientapp:secret.clientapp\
-  'http://localhost:9090/oauth/token?grant_type=password&username=user&password=user.password'
+  'http://localhost:9090/oauth/token?grant_type=authorization_code&code={YOUR_CODE}&scope=read&redirect_uri=http://localhost:9091'
 ```
--- src/main/java/com/leysoft/configuration/AuthorizationServerConfiguration.java
+++ src/main/java/com/leysoft/configuration/AuthorizationServerConfiguration.java
@@ -3,6 +3,7 @@ package com.leysoft.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +13,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import com.leysoft.util.GrantTypes;
 import com.leysoft.util.Scope;
@@ -21,12 +25,28 @@ import com.leysoft.util.Scope;
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Value(
+            value = "${jwt.signature}")
+    private String signatureKey;
+
+    @Value(
             value = "${oauth2.client.id}")
     private String clientId;
 
     @Value(
             value = "${oauth2.client.secret}")
     private String secretClient;
+
+    @Value(
+            value = "${oauth2.client.redirect-uri}")
+    private String redirectUrl;
+
+    @Value(
+            value = "${oauth2.client.access-token-validity-seconds}")
+    private int accessTokenValiditySeconds;
+
+    @Value(
+            value = "${oauth2.client.refresh-token-validity-seconds}")
+    private int refreshTokenValiditySeconds;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -39,14 +59,19 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager)
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
                 .userDetailsService(userDetailsService);
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient(clientId).secret(passwordEncoder.encode(secretClient))
-                .authorizedGrantTypes(GrantTypes.PASSWORD_CREDENTIALS.toString())
+                .redirectUris(redirectUrl)
+                .authorizedGrantTypes(GrantTypes.AUTHORIZATION_CODE.toString(),
+                        GrantTypes.REFRESH_TOKEN.toString())
+                .accessTokenValiditySeconds(accessTokenValiditySeconds)
+                .refreshTokenValiditySeconds(refreshTokenValiditySeconds)
                 .scopes(Scope.READ.toString());
     }
 
@@ -54,5 +79,17 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()")
                 .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signatureKey);
+        return converter;
     }
 }
